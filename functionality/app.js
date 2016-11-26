4dca5dd1dc5c7ae7417680d1cc4719870be795b0
viorelAlexandrescu <alex.viorel.117@gmail.com>
2016-11-26 14:28:27 +0200

    First Test! God help us...

--- functionality/app.js
+++ functionality/app.js
@@ -1,45 +1,57 @@
-var endPoint = "http://10.10.3.102/"
+var server = "http://10.10.3.102/";
+var userEmail;
 
-var checkLoginCredentials = (function (email, password) {
-    var hasBeenCalled = false;
-    return function () {
-        if (!hasBeenCalled) {
-            var checkCredentialsRequest = new XMLHttpRequest();
-            checkCredentialsRequest.onreadystatechange = function {
-                if (this.readyState == 4 && this.statusText == "OK") {
-                    // proceed to main page
-                }
-                else {
-                    var response = JSON.parse(this.responseText);
-                    console.log(response);
-                }
+function getUserCredentials(isForLogin) {
+    var emailForm = document.getElementById("emailTextField"),
+        passwordForm = document.getElementById("passwordTextField");
+    
+    if(!isForLogin){
+       var nameForm = document.getElementById("nameTextField");
+        return { 
+            email : emailForm.value,
+            password : passwordForm.value,
+            name : nameForm.value 
+        };
+    } else {
+        return { 
+            email : emailForm.value,
+            password : passwordForm.value,
+        };
+    }
+}
+
+
+
+function sendRequest(requestType, endpoint, data, doIUseCallback, successCallback, failureCallback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (doIUseCallback == true) {
+            var responseText = this.responseText;
+            if (this.readyState == 4 && this.status == 200) {
+                successCallback(responseText);
+            } else {
+                failureCallback(responseText);
             }
-            checkCredentialsRequest.open("GET", endPoint, true);
-            checkCredentialsRequest.send();
-            hasBeenCalled = true;
         }
     }
-    else {
-        console.log("PLS WAIT //LOGIN");
-    }
-})()
+    request.open(requestType, endpoint, true);
+    request.send(data);
+}
 
-var signUp = (function (email, password) {
-    var hasBeenCalled = false;
-    return function () {
-        if (!hasBeenCalled) {
-            var signUpRequest = new XMLHttpRequest();
-            signUpRequest.onreadystatechange = function {
-                if (this.readyState == 4 && this.status == 200) {
-                    // show message that user should check email and confirm
-                }
-            }
-            signUpRequest.open("GET", endPoint, true);
-            signUpRequest.send();
-            hasBeenCalled = true;
-        }
+function printServerResponseMessage(responseFromServer){
+    var response = JSON.parse(responseFromServer);
+    console.log(response.message);
+}    
+
+var onPageLoad = function () {
+    var loginButton = document.getElementById("loginButton"),
+        signUpButton = document.getElementById("signUpButton");
+    
+    loginButton.onclick = function () {
+        sendRequest("POST", server += "/login", getUserCredentials(true), true, printServerResponseMessage(), printServerResponseMessage());
     }
-    else {
-        console.log("PLS WAIT //SIGN UP");
+    signUpButton.onclick = function () {
+        sendRequest("POST", server += "/sign_up", getUserCredentials(false), true, printServerResponseMessage(), printServerResponseMessage());
     }
-})()
+}
+                    
